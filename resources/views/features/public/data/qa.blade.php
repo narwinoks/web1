5cb6602b49626e3586ebaa5d83c443df3ae5fb74
ramdanegie <ramdanegie@gmail.com>
2024-05-01 11:29:57 +0700

    style(qa) : font and margin

--- resources/views/features/public/data/qa.blade.php
+++ resources/views/features/public/data/qa.blade.php
@@ -6,8 +6,9 @@
         <div class="card-question mb-2 w-100">
             <div class="card-question-header">
                 <h6 class="card-question-title">
-                    <a class="text-muted text-uppercase" data-toggle="collapse" href="#question-{{ $key }}">
-                        <i class="fas fa-question-circle pr-3 mt-0-20 pull-left"></i>
+                    <a class="text-muted faq-head" data-toggle="collapse" href="#question-{{ $key }}">
+                        <i class="fas fa-question-circle pr-3 mt-0-20 pull-left" style="
+                        margin: 3px 5px 0 0;"></i>
                         {{ $data['question'] ?? 'Null' }}
                         <i class="fas fa-minus mr-2 text-slate pull-right"></i>
                     </a>
@@ -23,3 +24,14 @@
         </div>
     </div>
 @endforeach
+<style>
+.faq-head{
+    z-index: 1;
+    flex: 1;
+    position: relative;
+    font-weight: bold;
+    font-size: 15px;
+    color: #424242;
+}
+
+</style>
