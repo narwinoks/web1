6ee237cb770cfabe59a117cf91926807ad31aa0d
narwinoks <narnowin00@gmail.com>
2024-05-16 21:07:31 +0700

    core(main) :add meta data

--- resources/views/templates/public/head.blade.php
+++ resources/views/templates/public/head.blade.php
@@ -5,4 +5,8 @@
     <link href="{{ asset('assets/img/icon.png') }}" rel="shortcut icon" type="image/png">
     <title>@yield('title', \App\Helpers\Helper::profile('slug'))</title>
     @extends('templates.public.styles')
+    <meta name="description" content="Shutterbox is a professional Photographers service based in Tasikmalaya, offering high-quality photos for various events.">
+    <meta name="keywords" content="shutterbox,Photographers tasik,Photographers tasikmalaya">
+    <meta name="author" content="John Doe">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
